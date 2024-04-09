66ed81b8799776f17814ee27b4b55cff82ce96a3
NadyaPronina <113007223+NadyaPronina@users.noreply.github.com>
2024-04-09 19:16:59 +0300

    homework

--- pendulum.py
+++ pendulum.py
@@ -20,13 +20,13 @@ def symplectic_euler(func, x0, t):
         x[i] = [q_next, p_next]
     return x
 
-def symplectic_euler_param(func, x0, t, a):
+def symplectic_euler_param(func, x0, t, a, r, m, tau):
     x = np.zeros((len(t), len(x0)))
     x[0] = x0
     for i in range(1, len(t)):
         h = t[i] - t[i - 1]
         q_prev, p_prev = x[i - 1]
-        p_next = p_prev + h * func([q_prev, p_prev], 0, a)[1]
+        p_next = p_prev + h * func([q_prev, p_prev], 0, a, r, m, tau)[1]
         q_next = q_prev + h * p_next
         x[i] = [q_next, p_next]
     return x
@@ -34,10 +34,10 @@ def symplectic_euler_param(func, x0, t, a):
 g = 10
 L = 0.5
 dt = 1/240 # pybullet simulation step
-q0 = np.deg2rad(15)   # starting position (radian)
+q0 = np.pi - np.deg2rad(15)   # starting position (radian)
 jIdx = 1
-maxTime = 5
-logTime = np.arange(0.0, maxTime, dt)
+MaxTime = 0.1
+logTime = np.arange(0.0, MaxTime, dt)
 sz = len(logTime)
 logPos = np.zeros(sz)
 logPos[0] = q0
@@ -58,7 +58,7 @@ for _ in range(100):
 # turn off the motor for the free motion
 p.setJointMotorControl2(bodyIndex=boxId, jointIndex=jIdx, targetVelocity=0, controlMode=p.VELOCITY_CONTROL, force=0)
 for t in logTime[1:]:
-    # p.setJointMotorControl2(bodyIndex=boxId, jointIndex=jIdx, controlMode=p.TORQUE_CONTROL, force=0.1)
+    p.setJointMotorControl2(bodyIndex=boxId, jointIndex=jIdx, controlMode=p.TORQUE_CONTROL, force=0.1)
     p.stepSimulation()
 
     jointState = p.getJointState(boxId, jIdx)
@@ -66,17 +66,25 @@ for t in logTime[1:]:
     idx += 1
     logPos[idx] = th1
 
+
 def rp(x, t):
+    r = 0.5 
+    m = 2
+    tau = 0.1
     return [x[1], 
-            -g/L*np.sin(x[0])]
+            -g/L * np.sin(x[0]) -  (r * x[1])/(m * L**2) + tau / (m * L**2)] 
 
 def rp_lin(x, t):
+    r = 0.5 
+    m = 2
+    tau = 0.1
     return [x[1], 
-            -g/L*x[0]]
+            g/L * (x[0] - np.pi) -  (r * x[1])/(m * L**2) + tau / (m * L**2)]  
 
-def rp_param(x, t, a):
+
+def rp_param(x, t, a, r, m, tau):
     return [x[1], 
-            -a*np.sin(x[0])]
+            -a*np.sin(x[0]) -  (r * x[1])/(m * L**2) + tau / (m * L**2)]
 
 theta = odeint(rp, [q0, 0], logTime)
 logTheor = theta[:,0]
@@ -93,13 +101,18 @@ print(l2, linf)
 thetaLin = symplectic_euler(rp_lin, [q0, 0], logTime)
 logLin = thetaLin[:, 0]
 
-def l2_cost(a):
-    xx = symplectic_euler_param(rp_param, [q0, 0], logTime, a[0])
+def l2_cost(params):
+    a, r, m, tau = params
+    xx = symplectic_euler_param(rp_param, [q0, 0], logTime, a, r, m, tau)
     return cost(logPos-xx[:,0])[0]
 
 a0 = [0.9*g/L]
+r0 = 0.5
+m0 = 2
+tau0 = 0.1
+my_params = np.array([a0[0], r0, m0, tau0])
 print(f'init a: {a0}')
-res = minimize(l2_cost, a0)
+res = minimize(l2_cost, my_params)
 print(f'l2_res: {res.fun}')
 print(f'a: {res.x}')
 print(f'a_th: {g/L}')
@@ -111,7 +124,7 @@ plt.grid(True)
 plt.plot(logTime, logPos, label = "simPos")
 plt.plot(logTime, logTheor, label = "simTheor")
 plt.plot(logTime, logEuler, label = "simEuler")
-# plt.plot(logTime, logLin, label = "logLin")
+plt.plot(logTime, logLin, label = "logLin")
 plt.legend()
 
 plt.show()
